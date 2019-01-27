580f05a7f48c2737a06579d67e29295e65edf271
nicklocicero <nicklocicero@gmail.com>
2019-01-26 19:01:02 -0700

    set up static CSS, html, routs and 404 page

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-const http = require('http');
+const path = require('path');
 
-const routes = require('./routes');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer(routes);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-server.listen(3000);
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+
+// Catch all
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
